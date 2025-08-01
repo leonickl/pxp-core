3b4b2f65f06b53923685e8d9f42e01e6d0371350
Leo Nickl <leo@leonickl.de>
2025-08-01 18:23:49 +0200

    title

--- views/app.php
+++ views/app.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizze</title>
+    <title><?= config('title') ?></title>
     <style>
         /* Reset & base styles */
         * {
