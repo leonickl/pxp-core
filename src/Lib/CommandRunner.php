7a6bf4142fd082e68dc1680e459f18869ee8ee92
Leo Nickl <leo@leonickl.de>
2025-08-01 18:47:25 +0200

    command runner & index.php

---
+++ src/Lib/CommandRunner.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PXP\Core\Lib;
+
+class CommandRunner
+{
+    public function initDirs()
+    {
+        foreach (['database', 'log'] as $dir) {
+            $path = __DIR__ . '/' . $dir;
+
+            if (file_exists($path) && is_dir($path)) {
+                continue;
+            }
+
+            mkdir($path);
+        }
+    }
+
+    public function execute(?string $command = null, string ...$args)
+    {
+        if (!$command || $command === 'server') {
+            shell_exec('/usr/bin/php -S localhost:8085 '.path('vendor/leonickl/pxp-core/index.php'));
+        }
+
+        if ($command === 'migrate') {
+            require __DIR__.'/migrate.php';
+        }
+
+        if ($command === 'play') {
+            require __DIR__ . '/playground.php';
+        }
+
+        if ($command === 'trash') {
+            $table = @$args[1];
+
+            if(! $table) {
+                die('Please enter table' . PHP_EOL);
+            }
+
+            $trashed = DB::init()->trashedOnly($table);
+
+            foreach($trashed as $record) {
+                echo json_encode(array_values($record)), PHP_EOL;
+            }
+        }
+
+        if($command === 'restore') {
+            $table = @$args[1];
+
+            if(! $table) {
+                die('Please enter table' . PHP_EOL);
+            }
+
+            $id = @$args[2];
+
+            if(! $id) {
+                die('Please enter an id' . PHP_EOL);
+            }
+
+            DB::init()->restore($table, (int) $id);
+
+            echo "Restored $table with id $id.", PHP_EOL;
+        }
+
+    }
+}
