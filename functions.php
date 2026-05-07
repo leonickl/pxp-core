b0db89af67172a2fea7fd778aaba050ed958ce4e
Leo Nickl <leo@leonickl.de>
2026-05-07 15:29:23 +0200

    return type and format

--- functions.php
+++ functions.php
@@ -190,10 +190,11 @@ function url(): string
     return Router::path();
 }
 
-function uuid() {
+function uuid(): string
+{
     $data = random_bytes(16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+    $data[6] = chr(ord($data[6]) & 0x0F | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3F | 0x80);
 
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
